b62dff8774d2081b5e52e750e718e1eed3c3ba08
UchithmaSenevirathne <uchithma@gmail.com>
2025-03-22 20:52:53 +0530

    header completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import {IoHelpCircleSharp, IoNotificationsSharp} from "react-icons/io5";
+
+export const Header = () => {
+    return (
+        <div className="flex justify-between items-center w-full py-4 px-5 font-anek bg-[#FDFDFD] border-1 border-gray-200">
+            <div className="flex flex-col">
+                <h4 className="font-medium text-[14px] text-gray-700">Welcome,</h4>
+                <h1 className="font-semibold text-[17px]">Iman Adithya</h1>
+            </div>
+            <div className="flex items-center">
+                <IoNotificationsSharp  className="h-[28px] w-[28px] text-gray-600" />
+                <IoHelpCircleSharp  className="h-[34px] w-[54px] text-gray-600"/>
+            </div>
+        </div>
+    )
+}
--- src/layout/PrivateLayout.jsx
+++ src/layout/PrivateLayout.jsx
@@ -1,9 +1,13 @@
 import {SideBar} from "../components/SideBar.jsx";
+import {Header} from "../components/Header.jsx";
 
 export const PrivateLayout = () => {
     return (
         <div className="flex w-full h-screen">
             <SideBar/>
+            <div className="flex flex-col flex-1">
+                <Header/>
+            </div>
         </div>
     )
 }
